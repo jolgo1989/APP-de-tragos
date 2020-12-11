159b88a1a4aea5cc8ca8e8299d7578c2cdd34b01
Jolgo <jlgo60@hotmail.com>
2020-12-11 15:32:56 -0500

    Se avanzo en el segundo video

---
+++ app/src/main/java/com/example/appdetragos/data/Drink.kt
@@ -0,0 +1,6 @@
+package com.example.appdetragos.data
+
+data class Drink(
+    val imgen: String,
+    val nombre: String,
+    val descripcion: String )
---
+++ app/src/main/java/com/example/appdetragos/domain/Repo.kt
@@ -0,0 +1,6 @@
+package com.example.appdetragos.domain
+
+interface Repo {
+
+    fun getTragosList()
+}

7aa435d79704cb8dc20ad895a94018e6973184ca
Jolgo <jlgo60@hotmail.com>
2020-12-12 07:22:49 -0500

    Se finalizo segunda parte

--- app/src/main/java/com/example/appdetragos/MainActivity.kt
+++ app/src/main/java/com/example/appdetragos/MainActivity.kt
@@ -2,14 +2,22 @@ package com.example.appdetragos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.appdetragos.data.DataSource
+import com.example.appdetragos.domain.Repo
+import com.example.appdetragos.domain.RepoImpl
+import com.example.appdetragos.ui.viewmodel.MainViewModel
+import com.example.appdetragos.ui.viewmodel.VMFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+
+    private val viewModel by viewModels<MainViewModel>{ VMFactory(RepoImpl(DataSource()))}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

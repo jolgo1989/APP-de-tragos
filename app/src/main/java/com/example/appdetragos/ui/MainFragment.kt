7588b17a24934a0fae384ea534f35fbf578cb925
Jolgo <jlgo60@hotmail.com>
2020-12-13 09:20:12 -0500

    se agrego lista de img

--- app/src/main/java/com/example/appdetragos/ui/MainFragment.kt
+++ app/src/main/java/com/example/appdetragos/ui/MainFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.load.engine.Resource
 import com.example.appdetragos.R
 import com.example.appdetragos.data.DataSource
 import com.example.appdetragos.databinding.FragmentMainBinding
 import com.example.appdetragos.domain.RepoImpl
+import com.example.appdetragos.ui.viewmodel.MainAdapter
 import com.example.appdetragos.ui.viewmodel.MainViewModel
 import com.example.appdetragos.ui.viewmodel.VMFactory
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
 class MainFragment : Fragment() {
@@ -37,12 +42,30 @@ class MainFragment : Fragment() {
 
         binding = FragmentMainBinding.bind(view)
         setupReciclerView()
+        viewModel.fetchTragosList.observe(viewLifecycleOwner, Observer { result ->
+
+            when (result) {
+                is com.example.appdetragos.vo.Resource.Loading -> {
+                    //Metodo para el progresasBar(Loading)
+                    progressBar.visibility = View.VISIBLE
+                }
+                is com.example.appdetragos.vo.Resource.Success -> {
+                    progressBar.visibility = View.GONE
+                    recyclerView.adapter = MainAdapter(requireContext(),result.data)
+                }
+                is com.example.appdetragos.vo.Resource.Failure -> {
+                    progressBar.visibility = View.GONE
+                    Toast.makeText(requireContext(), "Ocurrio un erro al traer los datos${result.exception}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        })
 
     }
 
     private fun setupReciclerView() {
         binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        //metodo para color Dividir al RecyclerView
+        //metodo para color Divider al RecyclerView
         binding.recyclerView.addItemDecoration(
             DividerItemDecoration(
                 requireContext(),
---
+++ app/src/main/java/com/example/appdetragos/ui/viewmodel/MainAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.appdetragos.ui.viewmodel
+
+import android.content.Context
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.appdetragos.R
+import com.example.appdetragos.base.BaseViewHolder
+import com.example.appdetragos.data.Drink
+import kotlinx.android.synthetic.main.tragos_row.view.*
+
+class MainAdapter(private val context: Context, private val tragosList: List<Drink>) :
+    RecyclerView.Adapter<BaseViewHolder<*>>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
+        return MainViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.tragos_row, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        when (holder) {
+            is MainViewHolder -> holder.bind(tragosList[position])
+        }
+    }
+
+    override fun getItemCount(): Int = tragosList.size
+    inner class MainViewHolder(itemView: View) : BaseViewHolder<Drink>(itemView) {
+        override fun bind(item: Drink) {
+            Glide.with(context).load(item.imgen).into(itemView.img_cocktail)
+            itemView.txt_titulo.text = item.nombre
+            itemView.txt_descripcion.text = item.descripcion
+
+        }
+
+    }
+}
